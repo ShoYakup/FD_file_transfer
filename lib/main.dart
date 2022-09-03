5756040a2d00536fbe82ca26b412c9b6618f4392
Yakup <yakupgaylyiew@gmail.com>
2022-09-03 18:54:02 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
 
 
 
-fjsdgslksdgshjfksjsgasdksdgjskdfjnsdgsisgx jjnsdgffaidgsgansfbxbxb jrgsskrehnsegsju
+fjsdgslksdgshjfksjsgasdksdgjskdfjnsdgsisgx jjnsdgffaidgsgansfbxbxb jrgsskrehnsegsjukgtsrgkj
 
 
 git add .
